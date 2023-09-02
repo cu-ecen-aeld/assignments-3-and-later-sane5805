5e9b797e8f614369bed075bc7218ea7eb42d3560
Saurav Negi <112284209+sane5805@users.noreply.github.com>
2023-09-02 11:37:31 -0600

    Update Test_validate_username.c
    username comparison update in Test_validate_username function

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,14 @@
 */
 void test_validate_my_username()
 {
+    char * user_name = my_username();
+
+    char * user_name_from_config = malloc_username_from_conf_file();
+    
     /**
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    // TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(user_name, user_name_from_config, "Usernames do not match");
 }
