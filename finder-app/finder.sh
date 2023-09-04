07f056ef6c79abbbd9ef0da221b6b10ea3d106e8
Saurav Negi <112284209+sane5805@users.noreply.github.com>
2023-09-03 22:12:00 -0600

    Update finder.sh : taken out all debug messages

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -10,7 +10,7 @@ fi
 filesdir="$1"
 searchstr="$2"
 
-echo  "filesdir = $filesdir & searchstr = $searchstr" 
+#echo  "filesdir = $filesdir & searchstr = $searchstr" 
 
 # Check if filesdir exists and is a directory
 if [ ! -d "$filesdir" ]; then
@@ -22,16 +22,16 @@ fi
 # Find files in the directory and its subdirectories
 file_count=$(find "$filesdir" -type f | wc -l)
 
-echo "total file count is $file_count"
+#echo "total file count is $file_count"
 
 # Initialize a variable to count matching lines
 match_count=0
 
 # Debug
-echo "List of files to process:"
-find "$filesdir" -type f | while read -r file; do
-	echo "$file"
-done
+#echo "List of files to process:"
+#find "$filesdir" -type f | while read -r file; do
+#	echo "$file"
+#done
 
 # Loop through each file and search for the specified string
 find "$filesdir" -type f | while read -r file; do
