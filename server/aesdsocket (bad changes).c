0fbe0ada2547043aacdc0222f114ac1d934525c2
Saurav Negi <sane5805@colorado.edu>
2023-10-07 15:58:17 -0600

    testing commit

--- server/aesdsocket (bad changes).c
+++ server/aesdsocket (bad changes).c
@@ -8,13 +8,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h> // Include this header for inet_ntop
 
+#include <sys/types.h>
+#include <netdb.h>
+
 #define PORT 9000
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 
 int daemon_mode = 0;
 
 void sig_handler(int signo) {
-    //printf("\nCaught \n");
+    printf("\nCaught \n");
     if (signo == SIGINT || signo == SIGTERM) {
         syslog(LOG_INFO, "Caught signal, exiting");
         closelog();
@@ -24,54 +27,28 @@ void sig_handler(int signo) {
 }
 
 void daemonize() {
-    // Fork the parent process
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    }
-    if (pid > 0) {
-        // Parent process, exit
-        exit(EXIT_SUCCESS);
-    }
-
-    // Child process: create a new session and become the session leader
-    if (setsid() < 0) {
-        perror("setsid failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Fork again to prevent the process from acquiring a controlling terminal
-    pid = fork();
-    if (pid < 0) {
-        perror("Second fork failed");
-        exit(EXIT_FAILURE);
-    }
-    if (pid > 0) {
-        // Parent of the second fork, exit
-        exit(EXIT_SUCCESS);
-    }
-
-    // Change the working directory to a safe location
-    if (chdir("/") < 0) {
-        perror("chdir failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Close standard file descriptors (stdin, stdout, stderr)
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
+    // TODO: Implement daemonization
+    // ...
 }
 
 int main(int argc, char* argv[]) 
 {
+    struct sockaddr client_addr;
+    socklen_t client_addr_len;
+    int fd_accepted_soket;
+
+    struct sockaddr_in address_to_print;
+
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo; // will point to the results
+
     // Parse command-line arguments
     if (argc > 1 && strcmp(argv[1], "-d") == 0) {
         daemon_mode = 1;
     }
 
-    //printf("\nHello\n");
+    printf("\nHello\n");
 
     // Initialize syslog
     openlog("aesdsocket", LOG_PID, LOG_DAEMON);
@@ -85,65 +62,76 @@ int main(int argc, char* argv[])
         daemonize();
     }
 
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    int sockfd;
 
-    // Create socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+    sockfd = socket (PF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) 
+    {
         perror("Socket creation failed");
         return -1;
     }
 
-    // Bind socket to port 9000
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
 
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Socket bind failed");
-        close(server_socket);
+
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+    hints.ai_family = AF_UNSPEC; // don't catre IPv4 or IPv6
+    hints.ai_flags = AI_PASSIVE; // TCP stream sockets
+    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+
+    status = getaddrinfo(NULL, "9000", &hints, &servinfo);
+    if(status != 0) 
+    {
+        perror("setting up sockaddr failed");
         return -1;
     }
 
-    // Listen for incoming connections
-    if (listen(server_socket, 1) == -1) {
-        perror("Listen failed");
-        close(server_socket);
+    status = bind(sockfd, (struct sockaddr *)servinfo->ai_addr, servinfo->ai_addrlen);
+
+    if (status == -1) 
+    { 
+        perror("binding sockaddr failed");
+        return -1;
+    }
+
+
+    status = listen(sockfd, 1);
+    if (status == -1) 
+    { 
+        perror("listing for connections on socket failed");
         return -1;
     }
 
     while (1) {
         // Accept a connection
-        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
-        if (client_socket == -1) {
+        fd_accepted_soket = accept(sockfd, (struct sockaddr*)&client_addr, &client_addr_len);
+        if (fd_accepted_soket == -1) {
             perror("Accept failed");
-            close(server_socket);
+            //close(fd_accepted_soket);
             return -1;
         }
-
+        
+        socklen_t addr_size = sizeof(address_to_print);
         // Convert integer IP address to string format
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+        // status = getpeername(fd_accepted_soket, (struct sockaddr *)&address_to_print, &addr_size);
+        // syslog(LOG_INFO, "Accepted connection from %s\n", address_to_print);
+        // printf("Accepted connection from %s\n", address_to_print);
 
-
-        // Log accepted connection
-        syslog(LOG_INFO, "Accepted connection from %s", client_ip);
+        status = getpeername(fd_accepted_soket, (struct sockaddr *)&address_to_print, &addr_size);
+        syslog(LOG_INFO, "Accepted connection from %s\n", inet_ntoa(address_to_print.sin_addr));
+        printf("Accepted connection from %s\n", inet_ntoa(address_to_print.sin_addr));
 
         // Receive and append data
         char buffer[1024];
         FILE* data_file = fopen(DATA_FILE, "a");
         if (data_file == NULL) {
             perror("File open failed");
-            close(client_socket);
-            close(server_socket);
+            close(sockfd);
+            close(fd_accepted_soket);
             return -1;
         }
 
         ssize_t bytes_received;
-        while ((bytes_received = recv(client_socket, buffer, sizeof(buffer), 0)) > 0) {
+        while ((bytes_received = recv(fd_accepted_soket, buffer, sizeof(buffer), 0)) > 0) {
             fwrite(buffer, 1, bytes_received, data_file);
             if (strchr(buffer, '\n') != NULL) {
                 // Packet complete
@@ -157,8 +145,8 @@ int main(int argc, char* argv[])
         data_file = fopen(DATA_FILE, "r");
         if (data_file == NULL) {
             perror("File open failed");
-            close(client_socket);
-            close(server_socket);
+            // close(client_socket);
+            // close(server_socket);
             return -1;
         }
 
@@ -170,8 +158,8 @@ int main(int argc, char* argv[])
         if (file_data == NULL) {
             perror("Memory allocation failed");
             fclose(data_file);
-            close(client_socket);
-            close(server_socket);
+            // close(client_socket);
+            // close(server_socket);
             return -1;
         }
 
